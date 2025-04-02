d6daf7cdb4ebc6f0870929cc3797d70c59f75ab8
Grant Benson <gjbenson@ncsu.edu>
2025-04-02 13:02:00 -0400

    added location tracker and attempted to use session controller
    session controller gets mad at location tracker. I try to import session controller into location tracker but not importing module correctly

---
+++ SyncPapAR/Assets/GrantWork/LocationTracker.ts
@@ -0,0 +1,77 @@
+require('LensStudio:RawLocationModule');
+import { SessionController } from "../SpectacleSyncKit/Core/SessionController"; //TODO: fix path
+
+@component
+export class LocationTracker extends BaseScriptComponent {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  horizontalAccuracy: number;
+  verticalAccuracy: number;
+  timestamp: Date;
+  locationSource: string;
+
+  private repeatUpdateUserLocation: DelayedCallbackEvent;
+  private locationService: LocationService;
+  private hasStarted: boolean = false;
+
+  //on awake, start tracking once session controller starts up
+  onAwake() {
+    
+    // If already ready, start immediately
+    if (SessionController.getIsReady()) {
+      print("SessionController is already ready!");
+      this.initializeLocationTracking();
+      return;//don't call initializeLocationTracking twice
+    }
+    // if not ready, wait for SessionController to be ready before starting location tracking
+    SessionController.notifyOnReady(() => {
+      print("SessionController is finally ready!");
+      this.initializeLocationTracking();
+    });
+  }
+
+  initializeLocationTracking() {
+    if (this.hasStarted) return;
+    this.hasStarted = true;
+    this.createAndLogLocationAndHeading();
+  }
+
+  createAndLogLocationAndHeading() {
+    // Create location handler
+    this.locationService = GeoLocation.createLocationService();
+
+    // Set the accuracy
+    this.locationService.accuracy = GeoLocationAccuracy.Navigation;
+
+    this.repeatUpdateUserLocation = this.createEvent('DelayedCallbackEvent');
+    this.repeatUpdateUserLocation.bind(() => {
+      // Get users location.
+      this.locationService.getCurrentPosition(
+        (geoPosition) => {
+          if (
+            this.timestamp === undefined ||
+            this.timestamp.getTime() !== geoPosition.timestamp.getTime()
+          ) {
+            this.latitude = geoPosition.latitude;
+            this.longitude = geoPosition.longitude;
+            this.horizontalAccuracy = geoPosition.horizontalAccuracy;
+            this.verticalAccuracy = geoPosition.verticalAccuracy;
+            print('long: ' + this.longitude);
+            print('lat: ' + this.latitude);
+            this.timestamp = geoPosition.timestamp;
+          }
+        },
+        (error) => {
+          print(error);
+        }
+      );
+      
+      // Acquire next location update in 1 second
+      this.repeatUpdateUserLocation.reset(1.0);
+    });
+    
+    // Acquire next location immediately with zero delay
+    this.repeatUpdateUserLocation.reset(0.0);
+  }
+}
