5a71ae4d239e53d99cb6fa1d2a511ca59c556a2f
Grant Benson <gjbenson@ncsu.edu>
2025-04-02 12:18:37 -0400

    set up basic sync kit project

---
+++ PapAR/Cache/c5ba7661-1337-4fa3-9d5c-092e6524c36f/ebd5406bafad37bc6e1531469608fdb5/Data/LocationUpdate.ts
@@ -0,0 +1,61 @@
+require('LensStudio:RawLocationModule');
+
+@component
+export class LocationExample extends BaseScriptComponent {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  horizontalAccuracy: number;
+  verticalAccuracy: number;
+  timestamp: Date;
+  locationSource: string;
+
+  private repeatUpdateUserLocation: DelayedCallbackEvent;
+  private locationService: LocationService;
+  onAwake() {
+    this.createEvent('OnStartEvent').bind(() => {
+      this.createAndLogLocationAndHeading();
+    });
+
+    this.repeatUpdateUserLocation = this.createEvent('DelayedCallbackEvent');
+    this.repeatUpdateUserLocation.bind(() => {
+      // Get users location.
+      this.locationService.getCurrentPosition(
+        function (geoPosition) {
+          //Check if location coordinates have been updated based on timestamp
+          if (
+            this.timestamp === undefined ||
+            this.timestamp.getTime() != geoPosition.timestamp.getTime()
+          ) {
+            this.latitude = geoPosition.latitude;
+            this.longitude = geoPosition.longitude;
+            this.horizontalAccuracy = geoPosition.horizontalAccuracy;
+            this.verticalAccuracy = geoPosition.verticalAccuracy;
+            print('long: ' + this.longitude);
+            print('lat: ' + this.latitude);
+            if (geoPosition.altitude != 0) {
+              this.altitude = geoPosition.altitude;
+              print('altitude: ' + this.altitude);
+            }
+            this.timestamp = geoPosition.timestamp;
+          }
+        },
+        function (error) {
+          print(error);
+        }
+      );
+      // Acquire next location update in 1 second, increase this value if required for AR visualisation purposes such as 0.5 or 0.1 seconds
+      this.repeatUpdateUserLocation.reset(1.0);
+    });
+  }
+  createAndLogLocationAndHeading() {
+    // Create location handler
+    this.locationService = GeoLocation.createLocationService();
+
+    // Set the accuracy
+    this.locationService.accuracy = GeoLocationAccuracy.Navigation;
+
+    // Acquire next location immediately with zero delay
+    this.repeatUpdateUserLocation.reset(0.0);
+  }
+}
